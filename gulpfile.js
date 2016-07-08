40e4a9ec85a7916e0fa076b0bfb47ca2a686c16c
ha1f <ha1f@linecorp.com>
2016-07-08 18:07:27 +0900

    コメントメモを追加、微調整

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('browserify', function() {
     .bundle()
     .on("error", function (err) { console.log("Error : " + err.message); })
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./js/'))
 });
 
 gulp.task('watch', function() {
@@ -27,3 +27,44 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('default', ['browserify', 'watch', 'webserver']);
+
+/*
+http://qiita.com/hkusu/items/e068bba0ae036b447754
+とても参考
+
+http://umai-bow.hateblo.jp/entry/2014/10/08/002235
+より
+
+## browserify
+requireとかをみて、1ファイルにまとめてくれる
+
+#### .bundle()
+変換を実行
+
+.pipe(source('bundle.js'))
+vinyl-source-streamを使って、vinylに変換
+
+.pipe()
+streams.Transformを受け取って、変形をする
+
+gulp.src()
+vinylオブジェクトを作って返す
+
+
+uglifyを使うと小さくできるらしい？
+
+ES6 = ECMASCriptの6th Edition = ECMASCript2015 = ES2015
+
+Babel: トランスパイラ
+https://github.com/babel/babelify
+
+Bower
+あんま使われてないっぽい
+
+gulp.task('default', [task, task, task]);
+デフォルトのタスクを登録
+
+gulp.task('name', function(){})
+nameという名前でタスクを登録
+
+*/
